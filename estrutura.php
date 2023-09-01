68dfa572d89b8e6d704d4a7ae1f3c0542ad77f86
Vinícius de França Ferreira <vinicius.ferreira1999@hotmail.com>
2023-09-01 13:24:29 -0300

    clone-instagram

---
+++ estrutura.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+
+  <meta name="HandheldFriendly" content="true">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+
+  <title>Vinicius Ferreira (@viniferreiradev)</title>
+  <meta property="og:title" content="Vinicius Ferreira (@viniferreiradev)">
+
+  <meta name="description" content="Desenvolvedor Web Full Stack">
+  <meta itemprop="description" content="Desenvolvedor Web Full Stack">
+  <meta property="og:description" content="Desenvolvedor Web Full Stack">
+
+  <meta name="theme-color" content="#FFFFFF">
+  <meta name="msapplication-TileColor" content="#FFFFFF">
+  <meta name="msapplication-navbutton-color" content="#FFFFFF">
+  <meta name="apple-mobile-web-app-status-bar-style" content="#FFFFFF">
+
+  <link rel="shortcut icon" href="./assets/favicon/favicon.ico">
+
+  <meta property="og:locale" content="pt_BR">
+  <meta property="og:image:type" content="image/png">
+  <meta property="og:image:width" content="1080">
+  <meta property="og:image:height" content="1080">
+  <meta property="og:type" content="website">
+  <meta property="og:card" content="summary_large_image">
+
+  <meta name="image" content="./assets/favicon/favicon.png">
+  <meta itemprop="image" content="./assets/favicon/favicon.png">
+  <meta property="og:image" content="./assets/favicon/favicon.png">
+
+  <meta name="author" content="Instagram">
+  <meta itemprop="name" content="Instagram">
+  <meta name="article:author" content="Instagram">
+  <meta name="application-name" content="Instagram">
+  <meta property="og:site_name" content="Instagram">
+  <meta name="apple-mobile-web-app-title" content="Instagram">
+
+  <link rel="stylesheet" href="./assets/css/pages/index/index.css">
+
+</head>
+<body>
+
+  <!-- Nav -->
+  <nav>
+    <!-- Logo -->
+    <div class="logo">
+      <img src="./assets/favicon/favicon.png">
+    </div>
+    <!-- Opções -->
+    <div class="opcoes">
+      <!-- Página Inicial -->
+      <button>
+        <img src="./files/icone-casa.svg">
+        <div class="nome">
+          <p>Página Inicial</p>
+        </div>
+      </button>
+      <!-- Pesquisa -->
+      <button class="mobile">
+        <img src="./files/icone-busca.svg">
+        <div class="nome">
+          <p>Pesquisa</p>
+        </div>
+      </button>
+      <!-- Explorar -->
+      <button>
+        <img src="./files/icone-bussula.svg">
+        <div class="nome">
+          <p>Explorar</p>
+        </div>
+      </button>
+      <!-- Reels -->
+      <button>
+        <img src="./files/icone-reels.svg">
+        <div class="nome">
+          <p>Reels</p>
+        </div>
+      </button>
+      <!-- Mensagens -->
+      <button>
+        <img src="./files/icone-mensagem.svg">
+        <div class="nome">
+          <p>Mensagens</p>
+        </div>
+      </button>
+      <!-- Notificações -->
+      <button class="mobile">
+        <img src="./files/icone-coracao.svg">
+        <div class="nome">
+          <p>Notificações</p>
+        </div>
+      </button>
+      <!-- Mais -->
+      <button>
+        <img src="./files/icone-mais.svg">
+        <div class="nome">
+          <p>Criar</p>
+        </div>
+      </button>
+      <!-- Perfil -->
+      <button class="perfil">
+        <img src="./files/vinicius.jpeg">
+        <div class="nome">
+          <p>Perfil - Vinicius Ferreira</p>
+        </div>
+      </button>
+    </div>
+    <!-- Menu -->
+    <div class="menu">
+      <button>
+        <img src="./files/icone-menu.svg">
+        <div class="nome">
+          <p>Mais</p>
+        </div>
+      </button>
+    </div>
+  </nav>
+
+  <!-- Section -->
+  <section class="usuario">
+    <!-- Foto -->
+    <div class="foto">
+      <img src="./files/vinicius.jpeg">
+      <div class="circulo"></div>
+    </div>
+    <!-- Informações -->
+    <div class="informacoes">
+      <!-- Arroba -->
+      <div class="arroba">
+        <!-- Nome -->
+        <div class="nome">
+          <p>viniferreiradev</p>
+          <img src="./files/icone-selo.svg">
+        </div>
+        <!-- Botões -->
+        <div class="botoes">
+          <a href="https://instagram.com/viniferreiradev/"><button class="seguir">Seguir</button></a>
+          <button class="mensagem">Mensagem</button>
+          <button class="adicionar">
+            <img src="./files/icone-adicionar.svg">
+          </button>
+          <button class="opcoes">
+            <img src="./files/icone-bolinhas.svg">
+          </button>
+        </div>
+      </div>
+      <!-- Dados -->
+      <div class="dados">
+        <div class="publicacoes">
+          <h3>25</h3>
+          <p>publicações</p>
+        </div>
+        <div class="seguidores">
+          <h3>749</h3>
+          <p>seguidores</p>
+        </div>
+        <div class="seguindo">
+          <h3>3.091</h3>
+          <p>seguindo</p>
+        </div>
+      </div>
+      <!-- Bio -->
+      <div class="bio">
+        <h2>viniferreiradev</h2>
+        <p>Developer 👨🏻‍💻</p>
+        <a href="https:viniciusferreiradev.com">viniciusferreiradev.com</a>
+        <h3>Seguido(a) por <span>zuck, <a href="" style="color: #121212; font-size: 14px;">codecenterbrasil</a></span> e outras 39 pessoas</h3>
+      </div>
+    </div>
+  </section>
+
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,145 @@
+
+
+<?php include("estrutura.php"); ?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+
+  <link rel="stylesheet" href="./assets/css/pages/index/index.css">
+
+</head>
+<body>
+
+  <!-- Section -->
+  <section class="publicacoes">
+    <!-- Opções -->
+    <div class="opcoes">
+    <a href="index.php">
+      <button class="ativo">
+        <img src="./files/icone-feed.svg">
+        <p>PUBLICAÇÕES</p>
+      </button>
+      </a>
+      <a href="reels.php">
+      <button>
+        <img src="./files/icone-reels.svg">
+        <p>REELS</p>
+      </button>
+      </a>
+      <a href="marcados.php">
+      <button>
+        <img src="./files/icone-marcar.svg">
+        <p>MARCADOS</p>
+      </button>
+    </div>
+    </a>
+
+  </section>
+
+    <!-- Section -->
+    <section class="publicacoes">
+
+      <!-- Itens -->
+    <div class="itens">
+      <!-- Item -->
+      <div class="item">
+        <div class="tamanho">
+          <div class="informacao">
+            <img src="./assets/favicon/favicon.png">
+            <p>100</p>
+          </div>
+          <div class="imagem" style="background-image: url('./files/instagram7.jpg')"></div>
+        </div>
+      </div>
+      <!-- Item -->
+      <div class="item">
+        <div class="tamanho">
+          <div class="informacao">
+            <img src="./assets/favicon/favicon.png">
+            <p>100</p>
+          </div>
+          <div class="imagem" style="background-image: url('./files/instagram6.jpg')"></div>
+        </div>
+      </div>
+      <!-- Item -->
+      <div class="item">
+        <div class="tamanho">
+          <div class="informacao">
+            <img src="./assets/favicon/favicon.png">
+            <p>100</p>
+          </div>
+          <div class="imagem" style="background-image: url('./files/instagram8.jpg')"></div>
+        </div>
+      </div>
+      <!-- Item -->
+      <div class="item">
+        <div class="tamanho">
+          <div class="informacao">
+            <img src="./assets/favicon/favicon.png">
+            <p>100</p>
+          </div>
+          <div class="imagem" style="background-image: url('./files/instagram1.jpg')"></div>
+        </div>
+      </div>
+      <!-- Item -->
+      <div class="item">
+        <div class="tamanho">
+          <div class="informacao">
+            <img src="./assets/favicon/favicon.png">
+            <p>100</p>
+          </div>
+          <div class="imagem" style="background-image: url('./files/instagram2.jpg')"></div>
+        </div>
+      </div>
+      <!-- Item -->
+      <div class="item">
+        <div class="tamanho">
+          <div class="informacao">
+            <img src="./assets/favicon/favicon.png">
+            <p>100</p>
+          </div>
+          <div class="imagem" style="background-image: url('./files/instagram5.jpg')"></div>
+        </div>
+      </div>
+
+            <!-- Item -->
+            <div class="item">
+              <div class="tamanho">
+                <div class="informacao">
+                  <img src="./assets/favicon/favicon.png">
+                  <p>100</p>
+                </div>
+                <div class="imagem" style="background-image: url('./files/instagram6.jpg')"></div>
+              </div>
+            </div>
+
+                  <!-- Item -->
+      <div class="item">
+        <div class="tamanho">
+          <div class="informacao">
+            <img src="./assets/favicon/favicon.png">
+            <p>100</p>
+          </div>
+          <div class="imagem" style="background-image: url('./files/instagram7.jpg')"></div>
+        </div>
+      </div>
+
+            <!-- Item -->
+            <div class="item">
+              <div class="tamanho">
+                <div class="informacao">
+                  <img src="./assets/favicon/favicon.png">
+                  <p>100</p>
+                </div>
+                <div class="imagem" style="background-image: url('./files/instagram4.jpg')"></div>
+              </div>
+            </div>
+    </div>
+
+
+    
+  </section>
+
+</body>
+</html>
